3387fe4f8a05bd4757e9e32dc7f35eb7f05c75a1
LINE WORKS <57744269+lineworks-test@users.noreply.github.com>
2019-12-09 16:07:37 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -22,6 +22,6 @@ LOCAL_ADDRESS = "https://lwbottest-20191209-2.herokuapp.com/"
 # DB config
 DB_HOST = "ec2-174-129-226-232.compute-1.amazonaws.com"
 DB_NAME = "defcpktoib9mfb"
-DB_USER = "pkbeoptjpzydqgx"
+DB_USER = "pkbeoptjpzydqg"
 DB_PORT = "5432"
 DB_PASSWORD = "ad794dec7f7e268716d8f5fd033a8b51f74e22e8c4709e910fe2e15fbd9081a7"
